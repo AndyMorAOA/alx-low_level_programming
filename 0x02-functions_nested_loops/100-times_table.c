c1e73454cbaf6de9946fad186c890495fda87c2f
Andy Osei Asamoah <andy.osei96@gmail.com>
2023-02-21 18:05:19 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times_tablet - Prints the times table of the input
+ * print_times_table - Prints the times table of the input
  * starting with 0.
  * @n: the value of the times tables to be printed.
  */
